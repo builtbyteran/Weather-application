e41821c3b848c89d6327a0ee9167b1ac14ceacbb
Teran <teranarthur99@gmail.com>
2026-01-25 19:11:51 -0700

    Secure API key in config file

---
+++ config.example.js
@@ -0,0 +1,3 @@
+const CONFIG = {
+  apiKey: 'YOUR_API_KEY_HERE', // Get your key from openweathermap.org
+};
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var API_KEY = 'e31cab03c38e4dd38a4e113e1a1dbfcf';
+var API_KEY = CONFIG.API_KEY;
 var weather = [];
 var forecast = [];
 
-document.querySelector('.search').addEventListener('click', function() {
+document.querySelector('.search').addEventListener('click', function () {
   var city = document.querySelector('#search-query').value;
 
   document.querySelector('#search-query').value = '';
@@ -11,38 +11,42 @@ document.querySelector('.search').addEventListener('click', function() {
   fetchForecastData(city);
 });
 
-var fetchWeatherData = function(city) {
- const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&limit=1&units=imperial&appid=' + API_KEY;
+var fetchWeatherData = function (city) {
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    city +
+    '&limit=1&units=imperial&appid=' +
+    API_KEY;
 
- fetch(url, {
-  method: 'GET',
-  dataType: 'json'
- })
- .then(data => data.json())
- .then(data => addCurrentWeather(data))
-}
+  fetch(url, {
+    method: 'GET',
+    dataType: 'json',
+  })
+    .then((data) => data.json())
+    .then((data) => addCurrentWeather(data));
+};
 
-var addCurrentWeather = function(data) {
+var addCurrentWeather = function (data) {
   weather = [];
-  
+
   var weatherData = {
     temperature: Math.round(data.main.temp),
     city: data.name,
     weather: data.weather[0].main,
-    icon: data.weather[0].icon
-  }
+    icon: data.weather[0].icon,
+  };
 
-  weather.push(weatherData)
+  weather.push(weatherData);
 
   renderCurrentWeather();
-}
+};
 
-var renderCurrentWeather = function() {
+var renderCurrentWeather = function () {
   document.querySelector('.weather').replaceChildren();
 
   for (let i = 0; i < weather.length; i++) {
     const weatherData = weather[i];
-    
+
     var template = `
       <div class="container-fluid">
         <div class="row" id="render-weather">
@@ -59,62 +63,68 @@ var renderCurrentWeather = function() {
         </div>
       </div>`;
 
-    document.querySelector('.weather').insertAdjacentHTML('beforeend', template);
+    document
+      .querySelector('.weather')
+      .insertAdjacentHTML('beforeend', template);
   }
-}
+};
 
-var fetchForecastData = function(city) {
-  const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&limit=1&units=imperial&appid=' + API_KEY;
+var fetchForecastData = function (city) {
+  const url =
+    'https://api.openweathermap.org/data/2.5/forecast?q=' +
+    city +
+    '&limit=1&units=imperial&appid=' +
+    API_KEY;
 
   fetch(url, {
     method: 'GET',
-    dataType: 'json'
+    dataType: 'json',
   })
-  .then(data => data.json())
-  .then(data => addForecast(data))
-}
+    .then((data) => data.json())
+    .then((data) => addForecast(data));
+};
 
-var addForecast = function(data) {
+var addForecast = function (data) {
   forecast = [];
-  
-  var weekDay = new Intl.DateTimeFormat("en-us", {
-    weekday: 'long'
+
+  var weekDay = new Intl.DateTimeFormat('en-us', {
+    weekday: 'long',
   });
 
   var dayOne = {
     weather: data.list[0].weather[0].main,
     temperature: Math.round(data.list[0].main.temp),
     icon: data.list[0].weather[0].icon,
-    day: weekDay.format(new Date(data.list[0].dt_txt))
-  }
+    day: weekDay.format(new Date(data.list[0].dt_txt)),
+  };
 
   var dayTwo = {
     weather: data.list[7].weather[0].main,
     temperature: Math.round(data.list[7].main.temp),
     icon: data.list[7].weather[0].icon,
-    day: weekDay.format(new Date(data.list[7].dt_txt))
-  }
+    day: weekDay.format(new Date(data.list[7].dt_txt)),
+  };
 
   var dayThree = {
     weather: data.list[15].weather[0].main,
     temperature: Math.round(data.list[15].main.temp),
     icon: data.list[15].weather[0].icon,
-    day: weekDay.format(new Date(data.list[15].dt_txt))
-  }
+    day: weekDay.format(new Date(data.list[15].dt_txt)),
+  };
 
   var dayFour = {
     weather: data.list[23].weather[0].main,
     temperature: Math.round(data.list[23].main.temp),
     icon: data.list[23].weather[0].icon,
-    day: weekDay.format(new Date(data.list[23].dt_txt))
-  }
+    day: weekDay.format(new Date(data.list[23].dt_txt)),
+  };
 
   var dayFive = {
     weather: data.list[31].weather[0].main,
     temperature: Math.round(data.list[31].main.temp),
     icon: data.list[31].weather[0].icon,
-    day: weekDay.format(new Date(data.list[31].dt_txt))
-  }
+    day: weekDay.format(new Date(data.list[31].dt_txt)),
+  };
 
   forecast.push(dayOne);
   forecast.push(dayTwo);
@@ -123,14 +133,14 @@ var addForecast = function(data) {
   forecast.push(dayFive);
 
   renderForecast();
-}
+};
 
-var renderForecast = function() {
+var renderForecast = function () {
   document.querySelector('.forecast').replaceChildren();
 
   for (let i = 0; i < forecast.length; i++) {
     const forecastData = forecast[i];
-    
+
     var template = `
       <div class="col" id="render-forecast">
         <h2>${forecastData.weather}</h2>
@@ -139,6 +149,10 @@ var renderForecast = function() {
         <h4>${forecastData.day}</h4>
       </div>`;
 
-      document.querySelector('.forecast').insertAdjacentHTML('beforeend', template);
+    document
+      .querySelector('.forecast')
+      .insertAdjacentHTML('beforeend', template);
   }
-}
+};
+
+// https://github.com/projectshft/weather-project/pull/228
